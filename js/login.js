4bc4a2107b8da348f88defaaba0ebed5e32dc978
oonik <aniksarkar0177@gmail.com>
2022-10-20 15:49:12 +0600

    first commit

---
+++ js/login.js
@@ -0,0 +1,8 @@
+ document.getElementById('btn').addEventListener('click', function(){
+    const inputEmail = document.getElementById('email').value;
+    const inputPassword = document.getElementById('password').value;
+
+    if(inputEmail === 'aniksarkar0177@gmail.com' && inputPassword === '01779087'){
+        console.log('login succese');
+    }
+ })
